6cabc91312c20c849f114c79a177ded54b63cbba
Zachary Kennedy <zkennedy@student12.cse.nd.edu>
2025-04-22 12:37:33 -0400

    Initial Files

---
+++ disk.c
@@ -0,0 +1,79 @@
+/*
+CSE 30341 Spring 2025 Flash Translation Assignment.
+This is the flash translation layer.
+You should write all your code here.
+*/
+
+#include "disk.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+Structure of the flash translation layer.
+Go ahead and add or change things here as needed.
+*/
+
+struct disk {
+	struct flash_drive *flash_drive;
+	int nreads;
+	int nwrites;
+};
+
+/*
+Create a new flash translation layer for this flash drive f, and simulated number of blocks
+Go ahead and add or change things here as needed.
+*/
+
+struct disk * disk_create( struct flash_drive *f, int disk_blocks )
+{
+	struct disk *d = malloc(sizeof(*d));
+	d->flash_drive = f;
+	d->nreads = 0;
+	d->nwrites = 0;
+	return d;
+}
+
+/*
+Read a disk block through the flash translation layer.
+Go ahead and add or change things here as needed.
+*/
+
+int disk_read( struct disk *d, int disk_block, char *data )
+{
+	printf("disk_read: block %d\n",disk_block);
+
+	/* A dummy operation that won't get far: read the same page # as block # */
+	flash_read(d->flash_drive,disk_block,data);
+
+	d->nreads++;
+	return 0;
+}
+
+/*
+Write a disk block through the flash translation layer.
+Go ahead and add or change things here as needed.
+*/
+
+int disk_write( struct disk *d, int disk_block, const char *data )
+{
+	printf("disk_write: block %d\n",disk_block);
+
+	/* A dummy operation that won't get far: write the same page # as block # */
+	flash_write(d->flash_drive,disk_block,data);
+
+	d->nwrites++;
+	return 0;
+}
+
+/*
+Report the total number of operations performed.
+You can add more if you like here, but keep the display of reads and writes.
+*/
+
+void disk_report( struct disk *d )
+{
+	printf("\tdisk reads: %d\n",d->nreads);
+	printf("\tdisk writes: %d\n",d->nwrites);
+}
